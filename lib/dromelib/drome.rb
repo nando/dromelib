8a056e1edabd69460f49ae45eddf7e55e8f0141e
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2015-08-15 17:27:28 +0200

    More specs about opening a Drome

--- lib/dromelib/drome.rb
+++ lib/dromelib/drome.rb
@@ -1,15 +1,51 @@
 # Copyright 2015 The Cocktail Experience, S.L.
 module Dromelib
+  class DromeNotFoundError < ArgumentError; end
+
   # A place where any entry is welcome.
   class Drome
     attr_reader :name
 
     def initialize(name = :docudrome)
       @name = name
+      open
+    end
+
+    def self.open(name)
+      new name
+    end
+
+    def self.local_config_file(name)
+      "./config/dromes/#{name}.yml"
+    end
+
+    def self.gem_config_file(name)
+      File.dirname(__FILE__) + "/../../config/dromes/#{name}.yml"
     end
 
     def new_entry(auido)
       Dromelib::Entry.new(drome: self, auido: auido)
+    end
+
+    def method_missing(method, *args, &block)
+      @yaml[method.to_sym] || super
+    end
+
+    private
+
+    def open
+      config_file = config_filepath
+      fail DromeNotFoundError unless File.exist?(config_file)
+      @yaml = YAML.load_file(config_file)
+    end
+
+    def config_filepath
+      localfile = Drome.local_config_file(@name)
+      if File.exist?(localfile)
+        localfile
+      else
+        Drome.gem_config_file(@name)
+      end
     end
   end
 end
--- spec/drome/drome_spec.rb
+++ spec/drome/drome_spec.rb
@@ -2,15 +2,53 @@
 require_relative '../spec_helper'
 
 describe Dromelib::Drome do
-  let(:drome) { Dromelib::Drome.new }
+  let(:dromename) { :exampledrome }
+  let(:default_drome) { Dromelib::Drome.new }
+  let(:yaml_content) do
+    {
+      drome_name: 'Drome of Wadus'
+    }
+  end
 
-  it 'should have :docudrome as default name' do
-    drome.name.must_equal :docudrome
+  describe '.new' do
+    it 'should open the :docudrome by default' do
+      default_drome.name.must_equal Dromelib::Drome.open(:docudrome).name
+    end
+  end
+
+  describe '.open(dromename)' do
+    it 'should raise an exception dromename does not exists in config/dromes' do
+      proc do
+        Dromelib::Drome.open(dromename)
+      end.must_raise Dromelib::DromeNotFoundError
+    end
+
+    it 'should open the gem config file if not present locally' do
+      gem_filepath = Dromelib::Drome.gem_config_file(dromename)
+      File.stub(:exist?, true, gem_filepath) do
+        YAML.stub(:load_file, yaml_content, gem_filepath) do
+          drome = Dromelib::Drome.open(dromename)
+          drome.drome_name.must_equal yaml_content[:drome_name]
+        end
+      end
+    end
+
+    it 'should open the local config file if it is in config/dromes' do
+      local_filepath = Dromelib::Drome.local_config_file(dromename)
+      File.stub(:exist?, true, local_filepath) do
+        YAML.stub(:load_file, yaml_content, local_filepath) do
+          drome = Dromelib::Drome.open(dromename)
+          drome.drome_name.must_equal yaml_content[:drome_name]
+        end
+      end
+    end
   end
 
   describe '#new_entry' do
-    it 'should return an Entry instance' do
-      drome.new_entry('SemVer').must_be_kind_of Dromelib::Entry
+    it 'should return an Entry instance with the right drome inside' do
+      entry = default_drome.new_entry('SemVer')
+      entry.must_be_kind_of Dromelib::Entry
+      entry.drome.must_equal default_drome
     end
   end
 end
